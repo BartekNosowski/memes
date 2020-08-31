b58972e8360f6d8d195583ec5b8d952e09c0d2c6
Bartek <barteknosowski@gmail.com>
2020-08-31 15:01:40 +0200

    added gif details

--- src/main/java/com/example/memy/HomeController.java
+++ src/main/java/com/example/memy/HomeController.java
@@ -3,6 +3,9 @@ package com.example.memy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 
@@ -12,6 +15,24 @@ public class HomeController {
     public String getHome(ModelMap map){
         map.put("gifs",Gif.GIFS);
         return "home";
+    }
+    /* PathVariable zamienia automatycznie ze ścieżki, która się zmienia na zmienną
+       javovą o nazwie name
+       /gif/mike
+       String name = "mike";
+     */
+    // name - któe jest zmienny
+    @GetMapping("/gif/{name}")
+    public String getGif(@PathVariable String name, ModelMap map){
+
+        map.put("gif",Gif.GIFS.stream().filter(g->g.getName().equals(name)).findFirst().get());
+//        List<Gif> list = Gif.GIFS;
+//        for (Gif gif : list){
+//            if (gif.getName().equals(name))
+//                map.put("gif",gif);
+//            }
+
+        return "gif-details";
     }
 
 
