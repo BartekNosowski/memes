df26fd57ee89daee517586cf2ac9d6b8124d51d4
Bartek <barteknosowski@gmail.com>
2020-09-01 12:01:06 +0200

    added categories to html

--- src/main/java/com/example/memy/HomeController.java
+++ src/main/java/com/example/memy/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,10 +14,11 @@ import java.util.stream.Collectors;
 public class HomeController {
     // ModelMap służy do przekazywania zmiennej do html
     @GetMapping("/")
-    public String getHome(ModelMap map){
-        map.put("gifs",Gif.GIFS);
+    public String getHome(ModelMap map) {
+        map.put("gifs", Gif.GIFS);
         return "home";
     }
+
     /* PathVariable zamienia automatycznie ze ścieżki, która się zmienia na zmienną
        javovą o nazwie name
        /gif/mike
@@ -24,9 +26,9 @@ public class HomeController {
      */
     // name - któe jest zmienny
     @GetMapping("/gif/{name}")
-    public String getGif(@PathVariable String name, ModelMap map){
+    public String getGif(@PathVariable String name, ModelMap map) {
 
-        map.put("gif",Gif.GIFS.stream().filter(g->g.getName().equals(name)).findFirst().get());
+        map.put("gif", Gif.GIFS.stream().filter(g -> g.getName().equals(name)).findFirst().get());
 //        List<Gif> list = Gif.GIFS;
 //        for (Gif gif : list){
 //            if (gif.getName().equals(name))
@@ -35,16 +37,32 @@ public class HomeController {
 
         return "gif-details";
     }
+
     @GetMapping("/favorites")
-    public String getFavorites(ModelMap map){
-        map.put("gifs",Gif.GIFS.stream().filter(gif -> gif.isFavorite()).collect(Collectors.toList()));
+    public String getFavorites(ModelMap map) {
+        map.put("gifs", Gif.GIFS.stream().filter(gif -> gif.isFavorite()).collect(Collectors.toList()));
         return "favorites";
     }
+
     @GetMapping("/categories")
-    public String getCategories(ModelMap map){
+    public String getCategories(ModelMap map) {
+        map.put("categories", Category.categoryList);
         return "categories";
     }
+    // bez lambd
+    @GetMapping("/category/{name}")
+    public String getCategory(@PathVariable String name, ModelMap map) {
+
+        map.put("category", Category.categoryList.stream().filter(g -> g.getName().equals(name)).findFirst().get());
+        List<Gif> gifs = new ArrayList<>();
+        for (Gif gif: Gif.GIFS){
+            if (gif.getCategory().getName().equals(name)){
+                gifs.add(gif);
+            }
+        }
+        map.put("gifs",gifs);
+        return "category";
 
 
-
+    }
 }
